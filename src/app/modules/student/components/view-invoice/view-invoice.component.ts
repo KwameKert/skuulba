bc7cef90a2a589a5134492537b3af82916ffd933
kwamekert <tomriddle477@gmail.com>
2020-03-23 23:02:16 +0000

    Invoice component added

--- src/app/modules/student/components/view-invoice/view-invoice.component.ts
+++ src/app/modules/student/components/view-invoice/view-invoice.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-view-invoice',
@@ -10,6 +13,72 @@ export class ViewInvoiceComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.generatePdf();
   }
+
+  generatePdf(){
+   // const documentDefinition = { content: 'This is an sample PDF printed with pdfMake' };
+    pdfMake.createPdf(this.getDocumentDefinition()).open();
+  }  
+
+
+  getDocumentDefinition() {
+    
+     return {
+       content: [
+         {
+           text: 'INVOICE',
+           bold: true,
+           fontSize: 20,
+           alignment: 'center',
+           margin: [0, 0, 0, 20]
+         },
+         {
+           columns: [
+             [{
+               text: 'Hello',
+               style: 'name'
+             },
+             {
+               text: 'Hello'
+             },
+             {
+               text: 'Email : ' ,
+             },
+             {
+               text: 'Contant No : ',
+             },
+             {
+               text: 'GitHub: ',
+               link: 'this.resume.socialProfile',
+               color: 'blue',
+             }
+             ],
+             [
+              {
+                text: 'Hello'
+              },
+              {
+                text: 'Email : ' ,
+              },
+              {
+                text: 'Contant No : ',
+              },
+              {
+                text: 'GitHub: ',
+                link: 'this.resume.socialProfile',
+                color: 'blue',
+              }
+             ]
+           ]
+         }],
+         styles: {
+           name: {
+             fontSize: 16,
+             bold: true
+           }
+         }
+     };
+   }
 
 }
